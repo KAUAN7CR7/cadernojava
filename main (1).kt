dde6ab7c702760920096a4c93a1b04b430a7a134
KAUAN7CR7 <131807689+KAUAN7CR7@users.noreply.github.com>
2023-05-29 17:53:23 -0300

    Add files via upload

---
+++ main (1).kt
@@ -0,0 +1,19 @@
+fun main() {
+  println("Olá Mundo")
+  var nome = "Giovanna"
+ val nascimento = 2006
+
+println(nome)          // exibe o nome
+println(nascimento)     // exibir o nascimento
+val myNum = 5             // Int
+val myDoubleNum = 5.99    // Double
+val myLetter = 'D'        // Char
+val myBoolean = true      // Boolean
+val myText = "Hello"      // String
+
+  println (myNum)
+  println (myDoubleNum)
+  println (myLetter)
+  println (myBoolean)
+  println (myText)
+}
---
+++ main.kt
@@ -0,0 +1,17 @@
+fun main() {
+ //operador matematico
+  var x = 12
+  var y = 3
+  //exibir a soma de x e y
+  var soma=x+y
+  println("A soma é:"+soma)
+  //exibir a subtração de x e y
+  var sub=x-y
+  println("A subtração é:"+sub)
+  //exibir a multiplicação de x e y
+var mult=x*y
+  println("A multiplicação é:"+mult)
+  //exibir a divisão entre x e y
+  var div=x/y
+  println("A divisão é:"+div)
+}
