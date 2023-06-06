03d13e8b3e0bf7e7084752e4ba71f567468a0152
KAUAN7CR7 <131807689+KAUAN7CR7@users.noreply.github.com>
2023-06-06 17:53:36 -0300

    Add files via upload

--- main (5).kt
+++ main (5).kt
@@ -1,36 +1,15 @@
-class Main {
-  //cria um metodo(sem parametro)
-  static void impPrime(){
-    System.out.println("Só imprime isso");
+fun main(args: Arry<String>) {
+  //criar uma variavel do tipo booleana(falsa true)
+  var nomeVariavel: Boolean = false
+  println(nomeVariavel)
+  var x: Boolean = true
+  println(x)
+  //if faz teste e retona bloolean
+
+  if (x){
+     println("verdadeiro")
+  }
+  else{
+    println("falso")
   }
   
-  //aqui cria um método passando parametro
-  static void meuMetodo(String nome) {
-    System.out.println("O nome é: "+ nome);
-  }
-
-  //aqui cria um método passando parametro
-  static void nomeEidade(String nome,int ano) {
-    System.out.println(nome + " tem "+ano + " anos");
-  }
-
-  //crie um metodo chamado exib que exibe uma String na tela
-
-  //classe principal do java
-  public static void main (String[] args){
-    //chama metodo impPrime()
-    impPrime();
-
-    //chama metodo meuMetodo()
-    meuMetodo("Edney");
-    meuMetodo("Kauan");
-    meuMetodo("Thiago");
-
-    //metodo com dois parametros
-    nomeEidade("Edney", 48);
-    nomeEidade("Kauan", 17);
-    nomeEidade("Thiago", 18);
-   
-  }
-  
-}
