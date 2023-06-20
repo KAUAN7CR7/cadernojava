3a010bd795caef069e2df5cc6532432bb82a907d
KAUAN7CR7 <131807689+KAUAN7CR7@users.noreply.github.com>
2023-06-20 17:53:31 -0300

    Add files via upload

---
+++ main (3).kt
@@ -0,0 +1,14 @@
+fun  main ( args :  Array < String >) {
+  // cria uma variável do tipo Booleana(false true)
+  var nomeVariavel :  Boolean  =  false
+  println (nomeVariavel)
+   var x :  booleano  =  falso
+  println (x)
+
+  se (x){
+    println ( " verdadeiro " )
+  }
+  outra coisa {
+    println ( " falso " )
+  }
+
